65ab777d0f7e81d2c36d969bec964e6d5cc88811
pcuadros <patrick_jcq@hotmail.com>
2018-03-24 12:33:10 -0500

    cambios en la valoraciones

--- BIBLIOTECA/dbo/Tables/Valoración.sql
+++ BIBLIOTECA/dbo/Tables/Valoración.sql
@@ -2,8 +2,8 @@
     [Usuario_CodigoUsuario] INT      NOT NULL,
     [Libro_CodigoLibro]     INT      NOT NULL,
     [numerovaloracion]      INT      NULL,
-    [fechavaloracion]       DATETIME NULL,
-    [valorvaloracion]       INT      NULL,
+    [FechaValoracion]       DATETIME NOT NULL,
+    [ValorValoracion]       INT      NOT NULL,
     CONSTRAINT [valoración_pk] PRIMARY KEY CLUSTERED ([Usuario_CodigoUsuario] ASC, [Libro_CodigoLibro] ASC),
     CONSTRAINT [Valoración_Libro_FK] FOREIGN KEY ([Libro_CodigoLibro]) REFERENCES [dbo].[Libro] ([CodigoLibro]),
     CONSTRAINT [Valoración_Usuario_FK] FOREIGN KEY ([Usuario_CodigoUsuario]) REFERENCES [dbo].[Usuario] ([CodigoUsuario])
